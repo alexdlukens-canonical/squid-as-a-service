1bfae9a37a638a5d41a40d53aa36f186059a96b9
Alexander Lukens <alex.lukens@canonical.com>
2026-05-13 23:39:27 +0200

    re-implementing application with sonnet

--- charm/src/django_app/terrasquid/urls.py
+++
@@ -1,8 +0,0 @@
-"""terrasquid URL configuration."""
-from django.contrib import admin
-from django.urls import include, path
-
-urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("api/v1/", include("terrasquid.api.urls")),
-]
--- charm/src/django_app/terrasquid/wsgi.py
+++
@@ -1,8 +0,0 @@
-"""WSGI config for terrasquid project."""
-import os
-
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "terrasquid.settings")
-
-application = get_wsgi_application()
