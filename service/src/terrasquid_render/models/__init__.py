02140fb468c56c21cdcdfeb2c2ede556d4b8bea4
[bot] James Simpson <james.simpson@canonical.com>
2026-05-13 16:36:14 +0200

    T010: Create __init__.py for models package

---
+++ service/src/terrasquid_render/models/__init__.py
@@ -0,0 +1,13 @@
+"""Models package for terrasquid-render."""
+
+from terrasquid_render.models.base import (
+    AccessRule,
+    DestinationConfig,
+    ServiceDefinition,
+)
+
+__all__ = [
+    "AccessRule",
+    "DestinationConfig",
+    "ServiceDefinition",
+]
