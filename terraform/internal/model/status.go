20093d048910b74bd2c2767c5090a3f5a50efba1
Alexander Lukens <alex.lukens@canonical.com>
2026-06-08 16:19:27 -0400

    move terraform provider to own repo (prep for upload)

--- terraform/internal/model/status.go
+++
@@ -1,9 +0,0 @@
-package model
-
-type Status struct {
-	DBConfigVersion      int    `json:"db_config_version"`
-	AppliedConfigVersion int    `json:"applied_config_version"`
-	LastReload          string `json:"last_reload"`
-	LastReloadOK        bool   `json:"last_reload_ok"`
-	Unit                string `json:"unit"`
-}
